f200b187056dcdc99b2eb6468b46bb0e1791dfae
Deepa0912 <deepagowdadj14@gmail.com>
2026-05-11 13:23:48 +0530

    fixed vercel flask export

--- english-to-kannada/app.py
+++ english-to-kannada/app.py
@@ -53,5 +53,8 @@ def translate():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
+
+
+app = app
