60d1f17fb2cacc0c8bc9d665a87f9984081ad2b9
Deepa0912 <deepagowdadj14@gmail.com>
2026-05-11 12:56:46 +0530

    Added vercel deployment

--- english-to-kannada/app.py
+++ english-to-kannada/app.py
@@ -4,6 +4,16 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 LANGUAGES = {
     'kn': 'Kannada',
